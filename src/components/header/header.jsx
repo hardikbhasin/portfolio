29c53a2114a6958e82b26941ab632dc4d58a5da8
hardikbhasin <hardikbhasin398@gmail.com>
2023-05-05 19:07:39 +0530

    added header component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import './header.css'
 const header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header>
+      <div className="cotainer header_container">
+        <h5> Hello, I'm</h5>
+        <h1>Hardik Bhasin</h1>
+        <h5 className="text-light">a developer...</h5>
+      </div>
+    </header>
   )
 }
 
