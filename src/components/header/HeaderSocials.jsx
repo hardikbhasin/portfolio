d7c109e71e8a2500b6f5f8b3ff24f481460f968c
hardikbhasin <hardikbhasin398@gmail.com>
2023-05-06 15:43:41 +0530

    created headerSocials for social media links

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header_socials'>
+        <a href="https://www.linkedin.com/in/hardik-bhasin/" target='_blank'></a>
+        <a href="https://github.com/hardikbhasin" target='_blank'></a>
+        <a href="https://leetcode.com/hardikbhasin/" target='_blank'></a>
+      
+    </div>
+  )
+}
+
+export default HeaderSocials
