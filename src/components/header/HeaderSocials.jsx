1a6196552e3b600e67f8ba7262d1d046f3b8afde
hardikbhasin <hardikbhasin398@gmail.com>
2023-05-06 16:38:38 +0530

    added header css and fixed socials and scroll

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+import {SiLeetcode} from 'react-icons/si'
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-        <a href="https://www.linkedin.com/in/hardik-bhasin/" target='_blank'></a>
-        <a href="https://github.com/hardikbhasin" target='_blank'></a>
-        <a href="https://leetcode.com/hardikbhasin/" target='_blank'></a>
+        <a href="https://www.linkedin.com/in/hardik-bhasin/" target='_blank'><BsLinkedin /></a>
+        <a href="https://github.com/hardikbhasin" target='_blank'> <BsGithub /></a>
+        <a href="https://leetcode.com/hardikbhasin/" target='_blank'><SiLeetcode /></a>
       
     </div>
   )
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-// import ME from '../../assets/me.png';
+
+import HeaderSocial from './HeaderSocials'
 const header = () => {
   return (
     <header>
       <div className="cotainer header_container">
-        <h5> Hello, I'm</h5>
+        <h4> Hello, I'm</h4>
         <h1>Hardik Bhasin</h1>
-        <h5 className="text-light"> developer...</h5>
+        <h4 className="text-light"> developer...</h4>
         <CTA />
-        {/* <div className='me'>
-          <img src={ME} alt='me' />
-        </div> */}
+        <HeaderSocial />
         <a href='#contact' className='scroll_down'>Scroll down</a>
       </div>
     </header>
