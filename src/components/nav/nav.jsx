88305ec9180882b8f34c692e1d20f8268cabac1f
hardikbhasin <hardikbhasin398@gmail.com>
2023-05-14 19:42:23 +0530

    created nav with icons

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {MdContactPhone} from 'react-icons/md'
+import { BiBook } from 'react-icons/bi';
+import { BiMessageSquareDetail } from 'react-icons/bi';
+
 const nav = () => {
   return (
-    <div>
-      Nav
-    </div>
+    <nav>
+      <a href="#home"> <AiOutlineHome /></a>
+      <a href="#about"> <AiOutlineUser /></a>
+      <a href="#contact"> <MdContactPhone/></a>
+      <a href="#experience"> <BiBook /></a>
+      <a href="#portfolio"> < BiMessageSquareDetail /></a>
+    </nav>
   )
 }
 
