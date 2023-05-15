17ff32273299a498959b31a220daa96ffe8f138d
hardikbhasin <hardikbhasin398@gmail.com>
2023-05-15 19:48:26 +0530

    added image

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/about-me.jpg'
 const about = () => {
   return (
-   <section id='about'>About </section>
+    <section id='about'>
+      <h5>Get to know</h5>
+      <h2>About me</h2>
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={ME} alt='my-img'></img>
+          </div>
+
+        </div>
+        <div className="about_content">
+
+        </div>
+      </div>
+    </section>
   )
 }
 
