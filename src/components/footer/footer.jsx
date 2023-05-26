539e10421ef669f2f955f9af1aaa5e927143d93c
hardikbhasin <hardikbhasin398@gmail.com>
2023-05-26 20:46:14 +0530

    added footer.js

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+import {BsTwitter} from 'react-icons/bs'
+import {SiLeetcode} from 'react-icons/si'
+
+
 const footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer>
+      <a href='#' className='footer_logo'>Hardik Bhasin</a>
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Projects</a></li>
+        <li><a href='#qualification'>Qualifications</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+      <div className="footer_socials">
+        <a href='https://www.linkedin.com/in/hardik-bhasin/'><BsLinkedin /></a>
+        <a href='https://github.com/hardikbhasin'><BsGithub/></a>
+        <a href='https://twitter.com/hardik_bhasin_'><BsTwitter/></a>
+        <a href='https://leetcode.com/hardikbhasin/'><SiLeetcode/></a>
+      </div>
+
+      <div className="footer_copyright">
+        <small>&copy; Hardik Bhasin. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
