a4b8ca80b2919cc1d941eea0a01dade46dc110f5
hardikbhasin <hardikbhasin398@gmail.com>
2023-05-26 20:17:30 +0530

    emailjs in form

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './contact.css'
 import { MdOutlineEmail } from 'react-icons/md'
-import {BsWhatsapp} from 'react-icons/bs'
-import {BsLinkedin} from 'react-icons/bs'
+import { BsWhatsapp } from 'react-icons/bs'
+import { BsLinkedin } from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_2wwv1c9', 'template_h0qv7tr', form.current, '6Ba4633biHRNzIE6o')
+    e.target.reset();
+  };
   return (
     <section id='contact'>
       <h5>Get in touch</h5>
@@ -11,7 +20,7 @@ const Contact = () => {
       <div className="container contact_container">
         <div className="contact_options">
           <article className="contact_option">
-            <MdOutlineEmail className='contact_option-icons'/>
+            <MdOutlineEmail className='contact_option-icons' />
             <h4>Email</h4>
             <h5>hardikbhasin398@gmail.com</h5>
             <a href="mailto:hardikbhasin398@gmail.com" target='_blank' rel='noreferrer'>Send a message</a>
@@ -19,7 +28,7 @@ const Contact = () => {
           </article>
 
           <article className="contact_option">
-            <BsWhatsapp className='contact_option-icons'/>
+            <BsWhatsapp className='contact_option-icons' />
             <h4>WhatsApp</h4>
             <h5>+91-9140842553</h5>
             <a href="https://api.whatsapp.com/send?phone=+919140842553" target='_blank' rel='noreferrer'>Send a message</a>
@@ -27,7 +36,7 @@ const Contact = () => {
           </article>
 
           <article className="contact_option">
-            <BsLinkedin className='contact_option-icons'/>
+            <BsLinkedin className='contact_option-icons' />
             <h4>Linkedin</h4>
             {/* <h5>Hardik Bhasin</h5> */}
             <a href="https://www.linkedin.com/in/hardik-bhasin/" target='_blank' rel='noreferrer'>Send a message</a>
@@ -35,7 +44,7 @@ const Contact = () => {
           </article>
 
         </div>
-        <form action=''>
+        <form ref={form} onSubmit={sendEmail}>
           <input type='text' name='name' placeholder='Your full name' required></input>
           <input type='email' name='email' placeholder='Your email' required></input>
           <textarea name="message" placeholder='Type your message here' rows="7" required></textarea>
